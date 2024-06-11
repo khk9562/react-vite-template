6d9b6dc0a214b4aaf30677250cfbd3502732f551
stella <khk9562@naver.com>
2024-06-11 10:07:27 +0900

    절대경로 추가

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,9 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
     },
   },
 });
